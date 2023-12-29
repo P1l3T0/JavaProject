26e60e10a734f7d74647cf40781489d869faf900
Zornitsa Stoyanova <zorito_999@abv.bg>
2023-12-29 17:58:47 +0200

    feat(models): add owner model

--- JavaProjectBackend/src/main/java/com/JavaProject/JavaProjectBackend/Models/Owner.java
+++ JavaProjectBackend/src/main/java/com/JavaProject/JavaProjectBackend/Models/Owner.java
@@ -1,2 +1,24 @@
-package com.JavaProject.JavaProjectBackend.Models;public class Owner {
+package com.JavaProject.JavaProjectBackend.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Owner")
+public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+    @Column(name = "FirstName")
+    private String firstName;
+    @Column(name = "LastName")
+    private String lastName;
+    @Column(name = "Gym")
+    private String gym;
 }
