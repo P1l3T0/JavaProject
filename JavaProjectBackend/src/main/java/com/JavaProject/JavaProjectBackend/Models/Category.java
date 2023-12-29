4fd224071ad58766fb58e1d544e230be52d5ad7f
Zornitsa Stoyanova <zorito_999@abv.bg>
2023-12-29 18:10:16 +0200

    feat(models): add 3 new models

---
+++ JavaProjectBackend/src/main/java/com/JavaProject/JavaProjectBackend/Models/Category.java
@@ -0,0 +1,20 @@
+package com.JavaProject.JavaProjectBackend.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+    @Column(name = "Name")
+    private String name;
+}
---
+++ JavaProjectBackend/src/main/java/com/JavaProject/JavaProjectBackend/Models/Reviewer.java
@@ -0,0 +1,22 @@
+package com.JavaProject.JavaProjectBackend.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Reviewer")
+public class Reviewer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+    @Column(name = "FirstName")
+    private String firstName;
+    @Column(name = "LastName")
+    private String lastName;
+}
