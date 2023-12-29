1fcdca58b1288ee92c91519b29893440c89c2625
P1l3T0 <vgan3v@gmail.com>
2023-12-29 14:27:38 +0200

    feat(backend): add base models

---
+++ JavaProjectBackend/src/main/java/com/JavaProject/JavaProjectBackend/JavaProjectBackendApplication.java
@@ -0,0 +1,15 @@
+package com.JavaProject.JavaProjectBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaProjectBackendApplication.class, args);
+
+	}
+
+}
---
+++ JavaProjectBackend/src/test/java/com/JavaProject/JavaProjectBackend/JavaProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaProject.JavaProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
